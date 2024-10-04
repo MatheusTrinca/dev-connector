97c73af4ac3031808c2be5fd201361e7689c981f
Matheus <matheusfelipetrinca@gmail.com>
2024-10-04 18:07:42 -0300

    feat: adding-user-model

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('API running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
 
