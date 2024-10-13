8e146ec5679b236e305689499f5ed82d03f53018
Matheus <matheusfelipetrinca@gmail.com>
2024-10-13 03:02:19 -0300

    feat: adding-register-and-alert-reducer

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import { Provider } from 'react-redux';
+import store from './store';
+import Alert from './components/layout/Alert';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Alert />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
